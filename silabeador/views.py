83cb783f03e6bee30ac38c3ae4dbc2704824c74c
Marcos Alonso <marcosalonso@MBP-de-Marcos.home>
2021-06-04 00:23:17 +0200

    primeros pasos programa silabeador

---
+++ silabeador/views.py
@@ -0,0 +1,15 @@
+from silabeador import app
+from silabeador.tools import pilengua
+from flask import jsonify
+
+@app.route("/pilengua/<frase>")
+def enlenguapi(frase):
+    respuesta = pilengua(frase)
+    d = {
+        'original': frase, 
+        'pilengua': respuesta
+    }
+
+
+
+    return jsonify(d)
