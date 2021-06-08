d97897085a731e6f04b7586e32489f3b97ed81b3
Marcos Alonso <marcosalonso@MBP-de-Marcos.home>
2021-06-08 20:49:59 +0200

    creada ruta para base de datos de IMDB con un decorador en python

--- silabeador/views.py
+++ silabeador/views.py
@@ -1,6 +1,7 @@
 from silabeador import app
 from silabeador.tools import pilengua
 from flask import jsonify
+import requests
 
 @app.route("/pilengua/<frase>")
 def enlenguapi(frase):
@@ -9,7 +10,17 @@ def enlenguapi(frase):
         'original': frase, 
         'pilengua': respuesta
     }
-
-
-
     return jsonify(d)
+
+
+@app.route("/pelicula/<palabra>")
+def pelicula(palabra):
+    url = "http://www.omdbapi.com/?s={}&apikey=b8d84844"
+
+    resultado = requests.get(url.format(palabra))
+    if resultado.status_code == 200:
+        peliculas = resultado.json() #Dvuelve un diccionario del resultado, que contiene todas las peliculas que aparecen con la busqueda
+        if peliculas['Response'] == "False":
+            return jsonify({'status': "Error", "msg": "No se han encontrado resultados"})
+
+        return jsonify({"Peliculas": peliculas["Search"], 'status': "Success"})
